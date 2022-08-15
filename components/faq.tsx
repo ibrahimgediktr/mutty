1d62f9ca74866fc0cd4e7b26e4dfbc1a8c2f1738
Baris Ceviz <me@barisceviz.com>
2022-08-15 13:58:17 +0200

    fix: remove dummy faq content

--- components/faq.tsx
+++ components/faq.tsx
@@ -84,9 +84,6 @@ const useStyles = createStyles((theme, _params, getRef) => {
     };
 });
 
-const placeholder =
-    'It can’t help but hear a pin drop from over half a mile away, so it lives deep in the mountains where there aren’t many people or Pokémon.It was born from sludge on the ocean floor. In a sterile environment, the germs within its body can’t multiply, and it dies.It has no eyeballs, so it can’t see. It checks its surroundings via the ultrasonic waves it emits from its mouth.';
-
 export function FaqWithImage() {
     const {classes} = useStyles();
     return (
@@ -128,11 +125,6 @@ export function FaqWithImage() {
                             <Accordion.Panel>
                                 Open Pull Request to mutty source code repository. You can read how to add new keyword catalogs <a href={''}>This is detaild docs</a>
                             </Accordion.Panel>
-                        </Accordion.Item>
-
-                        <Accordion.Item className={classes.item} value="newsletter">
-                            <Accordion.Control>How can I subscribe to monthly newsletter?</Accordion.Control>
-                            <Accordion.Panel>{placeholder}</Accordion.Panel>
                         </Accordion.Item>
 
                         <Accordion.Item className={classes.item} value="credit-card">
